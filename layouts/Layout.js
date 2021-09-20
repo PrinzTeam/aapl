2d47755d11f198207ff2aefe6536b869cac74740
Elyeser <elyeser.f@gmail.com>
2021-09-19 23:59:10 -0400

    layout

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar";
 const Layout = ({ children }) => {
   return (
     <div>
-      <div className="min-h-screen z-50 fixed inset-0 pointer-events-none md:border-[10px] border-gray-900">
-        <div className="w-full h-full border-[10px] border-gray-900" />
-      </div>
+      <div className="h-screen z-50 fixed inset-0 pointer-events-none md:border-[10px] border-gray-900"></div>
+      <div className="fixed inset-[0.10rem]  pointer-events-none  border-4 border-white " />
+      <div className="fixed inset-[0.66rem] pointer-events-none  border-4 border-black " />
       <div className=" p-[25px] container">
         <Navbar /> <span className="px-4 md:px-0"> {children} </span>
       </div>
